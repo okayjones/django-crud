a42fbae802f8229f22a7b45c4e09ee0f4277213e
Logan Jones <loganemilyjones@mac.com>
2021-03-06 14:58:15 -0800

    use pytest

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Snack
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="unittester", email="test@email.com", password="pass"
+        )
+
+        self.snack = Snack.objects.create(
+            name="chips", description="I love chips", purchaser=self.user,
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), 'chips')
