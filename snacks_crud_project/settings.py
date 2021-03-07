7138abb961a4d4d9c67898245fb2d6af80d10a7b
Logan Jones <loganemilyjones@mac.com>
2021-03-07 12:55:30 -0800

    bootstrap styling

--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -44,8 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snacks.apps.SnacksConfig'
+    'snacks.apps.SnacksConfig',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
